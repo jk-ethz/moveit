c051a2fcee82cb9da2f082f0e043ea0d9ef97a88
JeroenDM <jeroendemaeyer@live.be>
2020-07-14 19:54:03 +0200

    no segfaults anymore! but still far from working

--- moveit_planners/ompl/ompl_interface/src/model_based_planning_context.cpp
+++ moveit_planners/ompl/ompl_interface/src/model_based_planning_context.cpp
@@ -114,8 +114,9 @@ void ompl_interface::ModelBasedPlanningContext::configure(const ros::NodeHandle&
   }
   complete_initial_robot_state_.update();
   ompl_simple_setup_->getStateSpace()->computeSignature(space_signature_);
-  ompl_simple_setup_->getStateSpace()->setStateSamplerAllocator(
-      std::bind(&ModelBasedPlanningContext::allocPathConstrainedSampler, this, std::placeholders::_1));
+  // ompl_simple_setup_->getStateSpace()->setStateSamplerAllocator(
+  //     std::bind(&ModelBasedPlanningContext::allocPathConstrainedSampler, this, std::placeholders::_1));
+  ompl_simple_setup_->getStateSpace()->allocDefaultStateSampler();
 
   // convert the input state to the corresponding OMPL state
   // ompl::base::ScopedState<> ompl_start_state(spec_.state_space_);
@@ -641,7 +642,7 @@ bool ompl_interface::ModelBasedPlanningContext::setGoalConstraints(
   }
 
   ob::GoalPtr goal = constructGoal();
-  ompl_simple_setup_->setGoal(goal);
+  // ompl_simple_setup_->setGoal(goal);
   return static_cast<bool>(goal);
 }
 
@@ -690,13 +691,13 @@ void ompl_interface::ModelBasedPlanningContext::preSolve()
   const ob::PlannerPtr planner = ompl_simple_setup_->getPlanner();
   if (planner && !multi_query_planning_enabled_)
     planner->clear();
-  startSampling();
+  // startSampling();
   ompl_simple_setup_->getSpaceInformation()->getMotionValidator()->resetMotionCounter();
 }
 
 void ompl_interface::ModelBasedPlanningContext::postSolve()
 {
-  stopSampling();
+  // stopSampling();
   int v = ompl_simple_setup_->getSpaceInformation()->getMotionValidator()->getValidMotionCount();
   int iv = ompl_simple_setup_->getSpaceInformation()->getMotionValidator()->getInvalidMotionCount();
   ROS_DEBUG_NAMED(LOGNAME, "There were %d valid motions and %d invalid motions.", v, iv);
